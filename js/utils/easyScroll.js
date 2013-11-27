77d04d2847af96b54713b829e72f6597187456a6
xiaomingming <lwm8817@126.com>
2013-11-27 18:13:46 +0800

    完成轮播js

--- js/utils/easyScroll.js
+++ js/utils/easyScroll.js
@@ -4,6 +4,7 @@
         pluginName = 'wordScroll';
 
     my[constructorFunName] = function(container, options) {
+        var self = this;
         this.container = container;
         var settings = $.extend({}, $.fn[pluginName].defaults, options);
         this.timer = null;
@@ -29,7 +30,33 @@
         this.isDirbtn = settings.isDirbtn;
         // 是否悬浮停止播放
         this.isHoverPause = settings.isHoverPause;
+        // 特效支持
+        // 包括移动展示，fadeIn fadeOut
+        this.effect = settings.effect;
+        // 移动方向
+        this.moveDirection = settings.moveDirection;
+        // 移动的宽度或者高度
+        this.moveLenConfig = {
+            'left': self.width,
+            'top': self.height
+        };
+        this.moveLen = this.moveLenConfig[this.moveDirection];
+        // 移动的动画配置
+        this.moveAnimateConfig={
+            currentE:{
+                animate:{},
+                css:{}
+            },
+            prevE:{
+                animate:{},
+                css:{}
+            }
+        };
 
+        this.moveAnimateConfig.currentE.animate[self.moveDirection]=0;
+        this.moveAnimateConfig.prevE.animate[self.moveDirection]=0;
+
+        this.moveAnimateConfig.currentE.css[self.moveDirection]=0;
         // 初始化
         this.init();
     };
@@ -54,7 +81,7 @@
         createSwitchWrapper: function() {
             if (!this.isSwitchWrapperCreated) {
                 this.isSwitchWrapperCreated = true;
-                return '<div class="switch-wrapper"></div>';
+                return '<div class="switch-wrapper" style="width:' + this.width + 'px;height:' + this.height + 'px"></div>';
             } else {
                 return false;
             }
@@ -89,10 +116,10 @@
         // 绑定数字播放事件
         playNumberEvent: function() {
             var self = this;
-            
+
             this.container.parent().find('.switch-number').on('click', 'a', function(e) {
                 e.preventDefault();
-                self.gotoIndex($(this).index(), self.startIndex);
+                self.gotoIndex($(this).index(), self.startIndex, '');
             });
         },
         // play number
@@ -100,14 +127,14 @@
             var self = this;
             this.container.parent().find('.switch-number').find('a').eq(index).addClass('current').siblings().removeClass('current');
         },
-        gotoIndex: function(index, prevIndex) {
+        gotoIndex: function(index, prevIndex, directionFlag) {
             // 停止轮播
             var self = this;
             this.stopSrcoll();
 
             // self.startIndex = index;
             // 
-            this.scroll(index, prevIndex);
+            this.scroll(index, prevIndex, directionFlag);
             this.autoScroll();
         },
 
@@ -135,7 +162,7 @@
             this.container.parent().find('.switch-prev').on('click', function(e) {
                 e.preventDefault();
                 clickIndex = self.getPrev(self.startIndex);
-                self.gotoIndex(clickIndex, self.startIndex);
+                self.gotoIndex(clickIndex, self.startIndex, -1);
             });
         },
         // 下一张按钮事件
@@ -145,13 +172,12 @@
             this.container.parent().find('.switch-next').on('click', function(e) {
                 e.preventDefault();
                 clickIndex = self.getNext(self.startIndex);
-                self.gotoIndex(clickIndex, self.startIndex);
+                self.gotoIndex(clickIndex, self.startIndex, 1);
             });
         },
         // get direction
         // 传入跳转后的下标，跳转之前的下标
         getDirection: function(gotoIndex, prevIndex) {
-            // console.log(gotoIndex-prevIndex);
             var res = gotoIndex - prevIndex;
             if (res >= 1) {
                 // 正向跳转
@@ -165,53 +191,72 @@
                 return 0;
             }
         },
-        // get Next
+        // get 前一张
         getPrev: function(index) {
             return (index === 0) ? (this.itemsLen - 1) : (index - 1);
         },
-        // 自动轮播下标控制
+        // 获取下一张
         getNext: function(index) {
             return (index + 1 === this.itemsLen) ? 0 : (index + 1);
         },
         // 获取移动的距离
         // 根据方向参数，自动播放/手动播放标识 来判断
-        getMoveDistance: function(index, prevIndex) {
-            return (this.timer) ? this.width : this.getDirection(index, prevIndex) * (this.width);
+        // 由于上一页，下一页按钮行为特殊，比如，下一页点到最后时，它的下一页就是起始，在两者跳转时，就不同于在分页上的点击那样跳转方向不一致，这个必须一致
+        getMoveDistance: function(index, prevIndex, directionFlag) {
+            var moveLen = this.moveLen;
+            if (directionFlag === '') {
+                //前进或后退，首尾图片切换方向不一致时
+                return (this.timer) ? moveLen : this.getDirection(index, prevIndex) * moveLen;
+            } else {
+                return directionFlag * moveLen;
+            }
+
         },
         // 滚动回调
-        scroll: function(index, prevIndex) {
-            // console.log(this.isAnimating);
+        scroll: function(index, prevIndex, directionFlag) {
             if (this.isAnimating) {
                 return;
             }
 
             this.isAnimating = true;
             // 更改开始的下标
+            // 这句相当关键，动画状态正在运动时，就不能让startIndex更改了，而放置的最佳位置，就是这里
             this.startIndex = index;
             var self = this,
-                moveDistance = this.getMoveDistance(index, prevIndex),
+                moveDistance = 0,
+                container = this.container,
+                currentEle = container.find('li').eq(index),
+                prevEle = container.find('li').eq(prevIndex),
                 promiseCurrent,
                 promisePrev;
-
             // 先移除current next类
-            this.container.find('li').removeClass('current prev');
+            container.find('li').removeClass('current prev');
 
-            promiseCurrent = this.container.find('li').eq(index).addClass('current').css({
-                'left': moveDistance + 'px'
-            }).stop(true, true).animate({
-                'left': 0
-            }, self.delay, 'linear', function() {
-                $(this).siblings().removeClass('prev').attr('style', '');
-                $(this).css('z-index', '1');
-            }).promise();
+            // 移动效果
+            if (this.effect === 'moveEffect') {
+                moveDistance = this.getMoveDistance(index, prevIndex, directionFlag);
+                self.moveAnimateConfig.currentE.css[self.moveDirection]=moveDistance+'px';
+                self.moveAnimateConfig.prevE.animate[self.moveDirection]=-moveDistance+'px';
 
-            // 再进行定性添加
-            promisePrev = this.container.find('li').eq(prevIndex).addClass('prev').stop(true, true).animate({
-                'left': -moveDistance + 'px',
-            }, self.delay, 'linear', function() {
-                $(this).attr('style', '');
-            }).promise();
+                // 当前
+                promiseCurrent = currentEle.addClass('current').css(self.moveAnimateConfig.currentE.css).stop(true, true).animate(self.moveAnimateConfig.currentE.animate, self.delay, 'linear', function() {
+                    $(this).siblings().removeClass('prev').attr('style', '');
+                    $(this).css('z-index', '1');
+                }).promise();
 
+                // 当前图片的前一个
+                promisePrev = prevEle.addClass('prev').stop(true, true).animate(self.moveAnimateConfig.prevE.animate, self.delay, 'linear', function() {
+                    $(this).attr('style', '');
+                }).promise();
+            }
+            // fade 效果
+            if (this.effect === 'fadeEffect') {
+                promiseCurrent = currentEle.stop(true, true).fadeIn(self.delay).promise();
+
+                promisePrev = prevEle.stop(true, true).fadeOut(self.delay).promise();
+            }
+            // 效果这里控制，本来使用key/value来进行控制，这样代码显得优雅
+            // 但是，在$.when()中，作为参数，产生了问题，于是这里代码就先ugly着
             $.when(promiseCurrent, promisePrev).done(function() {
                 self.isAnimating = false;
                 self.isPlayNumber && self.playNumber(index);
@@ -223,10 +268,8 @@
                 perveIndex;
             this.timer = setInterval(function() {
                 pervIndex = self.startIndex;
-                // console.log(pervIndex);
                 self.startIndex = self.getNext(self.startIndex);
-                // console.log(self.startIndex);
-                self.scroll(self.startIndex, pervIndex);
+                self.scroll(self.startIndex, pervIndex, 1);
             }, self.duration);
         },
         // 阻止滚动
@@ -236,14 +279,6 @@
                 clearInterval(self.timer);
                 self.timer = null;
             }
-        },
-        /*以下是可自定义回调*/
-        moveTo: function(left, top) {
-            this.container.parent().css({
-                'position': 'relative',
-                'left': left + 'px',
-                'top': top + 'px'
-            });
         }
     };
     $.fn[pluginName] = function(opts) {
@@ -267,6 +302,8 @@
 
     };
     $.fn[pluginName].defaults = {
+        'effect': 'moveEffect', // fadeEffect or moveEffect
+        'moveDirection': 'left', //left or top
         'isHoverPause': true,
         'isPlayNumber': true,
         'isDirbtn': true,
